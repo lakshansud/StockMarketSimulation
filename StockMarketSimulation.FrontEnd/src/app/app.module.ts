2975749a3f27f9ef172223b3e54b28157ef26632
lakshan <lakshan@alliontechnologies.com>
2018-06-03 19:36:26 +0530

    Added viwe model architecture

--- StockMarketSimulation.FrontEnd/src/app/app.module.ts
+++ StockMarketSimulation.FrontEnd/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { appRoutingProviders, routing } from './app.routing';
 import { NavComponent } from './nav/nav.component';
-import { DashBoardComponent } from './Dashboard/dashboard.component';
-import { dashboardRouting } from './Dashboard/dashboard.routing';
+import { DashBoardComponent } from './dashboard/dashboard.component';
+import { dashboardRouting } from './dashboard/dashboard.routing';
 
 @NgModule({
     declarations: [
---
+++ StockMarketSimulation.FrontEnd/src/app/models/BankAccount.ts
@@ -0,0 +1,5 @@
+export class BankAccount {
+    id: number;
+    Name: string;
+    Date: Date;
+}
