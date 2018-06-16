f55b5635be3151b8bed744725805418450109f8d
Shashika <Shashika@Shashikas-MacBook-Pro.local>
2018-06-16 19:34:09 +0530

    change function name

--- StockMarketSimulation.FrontEnd/src/assets/custom.js
+++ StockMarketSimulation.FrontEnd/src/assets/custom.js
@@ -1,18 +1,14 @@
-function test() {
+function analystPrediction() {
 
 var config = {
 	predictionSteps : 1,
 	step : 1,
 	serie : [
-		1,2,1,2,
-		1,5,1,3,
-		2,2,2,7,
-		4,2,9,2,
-		1,5,2,8,
+		1,2,1,2
 	]  
 }
 var prediction = brain_predict.predict(config) ;
 console.log("Prediction Value\n" + prediction.prediction + "\n");
 }
 
-test();
+analystPrediction();
