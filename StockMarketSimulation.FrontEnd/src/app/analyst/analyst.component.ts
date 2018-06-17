5b65c282ba2e1baf19f297a03a77cf5b5a8f114d
Shashika <Shashika@Shashikas-MacBook-Pro.local>
2018-06-17 10:37:00 +0530

    analyst testing

--- StockMarketSimulation.FrontEnd/src/app/analyst/analyst.component.ts
+++ StockMarketSimulation.FrontEnd/src/app/analyst/analyst.component.ts
@@ -6,4 +6,21 @@ import { Component } from '@angular/core';
 })
 export class AnalystComponent {
   title = 'app';
+
+	private config = {
+		predictionSteps : 1,
+		step : 1,
+		serie : [
+			1,2,1,2,
+			5,5,3,5
+		]  
+	}
+
+  constructor() {
+
+	var prediction = brain_predict.predict(this.config) ;
+		console.log("Prediction Value\n" + prediction.prediction + "\n");
+	}
+  
+
 }
--- StockMarketSimulation.FrontEnd/src/assets/custom.js
+++ StockMarketSimulation.FrontEnd/src/assets/custom.js
@@ -1,14 +1,15 @@
-function analystPrediction() {
+// function analystPrediction() {
 
-var config = {
-	predictionSteps : 1,
-	step : 1,
-	serie : [
-		1,2,1,2
-	]  
-}
-var prediction = brain_predict.predict(config) ;
-console.log("Prediction Value\n" + prediction.prediction + "\n");
-}
+// var config = {
+// 	predictionSteps : 1,
+// 	step : 1,
+// 	serie : [
+// 		1,2,1,2,
+// 		5,5,3,5
+// 	]  
+// }
+// var prediction = brain_predict.predict(config) ;
+// console.log("Prediction Value\n" + prediction.prediction + "\n");
+// }
 
-analystPrediction();
+// analystPrediction();
