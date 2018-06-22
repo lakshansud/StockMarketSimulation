919f0b0098e79f4eb3b32b09029ddba1509a08a7
lakshan <lakshan@alliontechnologies.com>
2018-06-22 21:10:03 +0530

    Fixed stock save issue

--- StockMarketSimulation.Api/src/java/Models/AnalystReturnViewModel.java
+++ StockMarketSimulation.Api/src/java/Models/AnalystReturnViewModel.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Repositories;
+package Models;
 
 import Models.AnalystViewModel;
 import java.util.ArrayList;
