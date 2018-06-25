e8c33cbe66747f5977e0ce2dd8d6d8b9dedf1519
shashikaxp <shashikaxp@gmail.com>
2018-06-25 23:30:36 +0530

    Re factor main method

--- StockMarketSimulation.GameHandler/src/Main.java
+++ StockMarketSimulation.GameHandler/src/Main.java
@@ -1,7 +1,17 @@
 
+import DatabaseConnection.DB;
+import GameHandler.ActiveUsers;
+import GameHandler.EventGenerator;
+import GameHandler.PlayerAI;
 import GameHandler.PriceChangeStock;
 import GameHandler.ThreadForValChange;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -14,18 +24,61 @@ import java.util.List;
  */
 public class Main {
 
+    static TimerTask staticTask;
+
     public static void main(String[] args) throws InterruptedException {
+
+        PlayerAI ai = new PlayerAI();
+        ai.botPlayerInteraction();
+
+        TimerTask task = new TimerTask() {
+
+            @Override
+
+            public void run() {
+                try {
+                    startRound();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+        };
+
+        staticTask = task;
+
+        Timer timer = new Timer();
+        long delay = 0;
+        long intevalPeriod = 1 * 2000;
+
+        // schedules the task to be run in an interval 
+        timer.scheduleAtFixedRate(task, delay, intevalPeriod);
+
+    }
+
+    public static void startRound() throws InterruptedException {
+
+        ActiveUsers ac = new ActiveUsers();
         
-      
-        int turnRounds = 5;
+        int turnNo = 5;
         int turnTime = 10000;
-        
-        for (int i = 1; i <= turnRounds; i++) {
-                ThreadForValChange tf = new ThreadForValChange(i);
+        if (ac.checkActivatedUsers()) {
+            System.out.println("Active users");
+            for (int i = 1; i <= turnNo; i++) {
+                ThreadForValChange tf = new ThreadForValChange(i);   
                 tf.sleep(turnTime);
                 System.out.println("Turn " + i);
                 tf.start();
+                staticTask.cancel();
+                if (i == turnNo) {
+                    ac.deactivateAllUsers();
+                }
             }
+        } else {
+            System.out.println("No active users");
         }
+    }
+
+
 
 }
